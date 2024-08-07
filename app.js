76b6466a7a5d3091126daeb2d6126daa618e5f3d
maheeen-khan <maheeenkhan4@gmail.com>
2024-08-07 20:15:09 +0500

    changes done

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ function addTask(){
 
     var task = document.createElement('div');
     
+    task.style.backgroundColor = 'white';
+    task.style.borderRadius = '6px';
+    task.style.padding = '6px';
+    task.style.paddingLeft = '12px';
+    task.style.textAlign = 'left';
     task.innerHTML = `${todo.value}`;
 
     document.getElementById("tasks").appendChild(task);
